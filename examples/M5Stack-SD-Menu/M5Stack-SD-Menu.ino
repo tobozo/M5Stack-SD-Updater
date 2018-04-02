2360ba572ce5c3d1c00c567eb79d1358afd0f37a
tobozo <tobozo@users.noreply.github.com>
2018-04-02 21:12:29 +0200

    Update M5Stack-SD-Menu.ino

--- examples/M5Stack-SD-Menu/M5Stack-SD-Menu.ino
+++ examples/M5Stack-SD-Menu/M5Stack-SD-Menu.ino
@@ -69,7 +69,8 @@
 #include "M5StackUpdater.h"      // https://github.com/tobozo/M5Stack-SD-Updater
 #include <M5StackSAM.h>          // https://github.com/tomsuch/M5StackSAM
 #include <ArduinoJson.h>         // https://github.com/bblanchon/ArduinoJson/
-#include "qrcode.h"              // https://github.com/ricmoo/qrcode
+// uncomment this if your M5Stack library isn't up to date
+//#include "qrcode.h"              // https://github.com/ricmoo/qrcode
 #include "i18n.h"                // language file
 #include "assets.h"              // some artwork for the UI
 
