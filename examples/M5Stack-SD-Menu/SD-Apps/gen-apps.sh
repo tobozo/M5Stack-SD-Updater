a5953856d2fea29e639ba8f899b6ba503e9981dd
tobozo <tobozo@users.noreply.github.com>
2018-04-14 19:53:59 +0200

    exported travis compilation script

--- examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
@@ -5,6 +5,7 @@ for D in *; do
   if [ -d "${D}" ]; then
     echo "moving to ${D}";
     cd ${D};
+    ls -la
     egrep -R M5StackUpdater && egrep -R updateFromFS && export m5enabled=1 || export m5enabled=0;
     if (( $m5enabled == 1 )); then
       export PATH_TO_INO_FILE="$(find . -type f -iname *.ino)";
