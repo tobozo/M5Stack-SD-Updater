d8c054e3cbfc45e23d1095c5d55c95c2de335bc2
tobozo <tobozo@users.noreply.github.com>
2018-04-14 19:41:27 +0200

    exported travis compilation script

--- examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
@@ -3,15 +3,16 @@
 
 for D in *; do
   if [ -d "${D}" ]; then
-    echo "${D}";
+    echo "moving to ${D}";
     cd ${D};
     egrep -R M5StackUpdater && egrep -R updateFromFS && export m5enabled=1 || export m5enabled=0;
     if (( $m5enabled == 1 )); then
       export PATH_TO_INO_FILE="$(find . -type f -iname *.ino)";
-      echo $PATH_TO_INO_FILE;
-      arduino --verbose-build --verify --board $BOARD $PATH_TO_INO_FILE;
+      echo "Compiling ${PATH_TO_INO_FILE}";
+      arduino --verbose-build --verify --board $BOARD $PATH_TO_INO_FILE >> $SDAPP_FOLDER/out.log;
+      ls $TRAVIS_BUILD_DIR/build -la
     else
-      echo bof;
+      echo "Not compiling ${PATH_TO_INO_FILE} as it needs injection first";
     fi
     cd ..
   fi
