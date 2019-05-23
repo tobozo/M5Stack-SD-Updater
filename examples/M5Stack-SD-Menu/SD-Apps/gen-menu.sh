93b3f4b5a97c744944c09c04971067fe8312fc68
tobozo <tobozo@users.noreply.github.com>
2019-05-23 12:36:22 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/gen-menu.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-menu.sh
@@ -5,15 +5,15 @@ export outfile=$SDAPP_FOLDER/../downloader.h
 
 [ -f $inofile ] && echo "Compiling $inofile"
 
-[ -f $inofile ] && arduino --preserve-temp-files --verbose-build --verify $PWD/examples/$EXAMPLE/$EXAMPLE.ino &>/dev/null
+[ -f $inofile ] && arduino --preserve-temp-files --verbose-build --verify $inofile &>/dev/null
 [ -f $inofile ] && find /tmp -name \*.partitions.bin -exec rm {} \; #
 [ -f $inofile ] && find /tmp -name \*.bin -exec mv {} $M5_SD_BUILD_DIR/TobozoLauncher.bin \; #
 [ -f $inofile ] && cp $M5_SD_BUILD_DIR/TobozoLauncher.bin $M5_SD_BUILD_DIR/menu.bin
 
-[ -f $inofile ] && echo "Compiling $outfile"
+[ -f $outfile ] && echo "Compiling Beta $inofile"
 
 [ -f $outfile ] && sed -i -e 's/"\/sd-updater";/"\/sd-updater\/unstable"/g' $outfile
-[ -f $outfile ] && arduino --preserve-temp-files --verbose-build --verify $PWD/examples/$EXAMPLE/$EXAMPLE.ino &>/dev/null
+[ -f $outfile ] && arduino --preserve-temp-files --verbose-build --verify $inofile &>/dev/null
 [ -f $outfile ] && find /tmp -name \*.partitions.bin -exec rm {} \; #
 [ -f $outfile ] && find /tmp -name \*.bin -exec mv {} $M5_SD_BUILD_DIR/BetaLauncher.bin \; # 
 
