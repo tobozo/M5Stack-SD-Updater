cca13f46af5e350089fbe79e24d2a0c42a56c8eb
tobozo <tobozo@users.noreply.github.com>
2019-05-23 16:50:55 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/after_deploy.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/after_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -v -H "Authorization: token $GH_TOKEN" --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases" | jq -r ".[].draft"
+curl -v -H "Authorization: token $GH_TOKEN" --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases" | jq -r ".[] | select(.tag_name==\"$TRAVIS_TAG\")" | jq ".assets[] | select(.name=\"$ARCHIVE_ZIP\") | .browser_download_url"
+
 
