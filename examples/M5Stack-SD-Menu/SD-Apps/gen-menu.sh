1db2dd36cf7da239dbdbf1f6b50edee0c1d963d9
tobozo <tobozo@users.noreply.github.com>
2019-05-23 10:14:17 +0200

    Preparing unstable channel

---
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+arduino --preserve-temp-files --verbose-build --verify $PWD/examples/$EXAMPLE/$EXAMPLE.ino &>/dev/null
+find /tmp -name \*.partitions.bin -exec rm {} \; #
+find /tmp -name \*.bin -exec mv {} $M5_SD_BUILD_DIR/TobozoLauncher.bin \; #
+cp $M5_SD_BUILD_DIR/TobozoLauncher.bin $M5_SD_BUILD_DIR/menu.bin
+
+export $outfile = $PWD/examples/$EXAMPLE/downloader.h
+[ -f $outfile ] && sed -i -e 's/"\/sd-updater";/"\/sd-updater\/unstable"/g' $outfile
+[ -f $outfile ] && arduino --preserve-temp-files --verbose-build --verify $PWD/examples/$EXAMPLE/$EXAMPLE.ino &>/dev/null
+[ -f $outfile ] && find /tmp -name \*.partitions.bin -exec rm {} \; #
+[ -f $outfile ] && find /tmp -name \*.bin -exec mv {} $M5_SD_BUILD_DIR/menu_beta.bin \; # 
+
+echo "Fake Binary" >> $M5_SD_BUILD_DIR/Downloader.bin
+echo "Main APPs Compilation successful, now compiling deps"
