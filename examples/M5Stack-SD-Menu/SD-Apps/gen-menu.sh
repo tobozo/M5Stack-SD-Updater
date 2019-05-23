5854e86fd41309874127d935886a143efc9ffb16
tobozo <tobozo@users.noreply.github.com>
2019-05-23 10:31:07 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/gen-menu.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-menu.sh
@@ -5,7 +5,7 @@ find /tmp -name \*.partitions.bin -exec rm {} \; #
 find /tmp -name \*.bin -exec mv {} $M5_SD_BUILD_DIR/TobozoLauncher.bin \; #
 cp $M5_SD_BUILD_DIR/TobozoLauncher.bin $M5_SD_BUILD_DIR/menu.bin
 
-export $outfile=$PWD/examples/$EXAMPLE/downloader.h
+export outfile=$PWD/examples/$EXAMPLE/downloader.h
 [ -f $outfile ] && sed -i -e 's/"\/sd-updater";/"\/sd-updater\/unstable"/g' $outfile
 [ -f $outfile ] && arduino --preserve-temp-files --verbose-build --verify $PWD/examples/$EXAMPLE/$EXAMPLE.ino &>/dev/null
 [ -f $outfile ] && find /tmp -name \*.partitions.bin -exec rm {} \; #
