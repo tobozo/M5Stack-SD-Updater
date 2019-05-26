73b9c64e245501a71a7efa85ee20dff936d442e3
tobozo <tobozo@users.noreply.github.com>
2019-05-26 16:35:02 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/install.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ -f $SDAPP_FOLDER/install_$TRAVIS_BRANCH.sh ] then;
+if [ -f $SDAPP_FOLDER/install_$TRAVIS_BRANCH.sh ]; then
   source $SDAPP_FOLDER/install_$TRAVIS_BRANCH.sh
 else
   echo "No install script for this branch"
