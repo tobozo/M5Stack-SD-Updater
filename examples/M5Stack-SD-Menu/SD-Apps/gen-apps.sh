bd73bb21a94f195c72d2c8e34af8c5be5d93eadb
tobozo <tobozo@users.noreply.github.com>
2019-05-26 17:56:48 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
@@ -5,13 +5,14 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
   # only rebuild all when master is updated
   echo "Skipping rebuild, will download last binaries"
   export LAST_SDAPP_FILE="SD-Apps-Folder.zip"
-  curl --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases/latest?access_token=$GH_TOKEN" | jq -r ".assets[0].browser_download_url" | wget --output-document=$LAST_SDAPP_FILE -i -
-  if [ ! -f $LAST_SDAPP_FILE ]; then
-    echo "Could not find a valid $LAST_SDAPP_FILE from latest releases, time to tune up jq queries ?"
+  #curl --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases/latest?access_token=$GH_TOKEN" | jq -r ".assets[0].browser_download_url" | wget --output-document=$LAST_SDAPP_FILE -i -
+  curl -H "Authorization: token $GH_TOKEN" --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases" | jq -r ".[] | select(.tag_name==\"unstable\")" | jq -r ".assets[] | select(.name==\"$ARCHIVE_ZIP\")  .browser_download_url"  | wget --output-document=$ARCHIVE_ZIP -i -
+  if [ ! -f $ARCHIVE_ZIP ]; then
+    echo "Could not find a valid $ARCHIVE_ZIP from latest releases, time to tune up jq queries ?"
     exit 1
   fi
-  unzip -d /tmp/$LAST_SDAPP_FILE $LAST_SDAPP_FILE
-  cp -Ruf /tmp/$LAST_SDAPP_FILE/* $M5_SD_BUILD_DIR/
+  unzip -d /tmp/$ARCHIVE_ZIP $ARCHIVE_ZIP
+  cp -Ruf /tmp/$ARCHIVE_ZIP/* $M5_SD_BUILD_DIR/
   exit 0
 fi
 
