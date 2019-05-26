bfc31990bfb6b5c18006715564432d950a96be12
tobozo <tobozo@users.noreply.github.com>
2019-05-26 18:37:43 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  # only rebuild all when master is updated
-  echo "Skipping rebuild, will download last binaries"
-  export LAST_SDAPP_FILE="SD-Apps-Folder.zip"
-  #curl --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases/latest?access_token=$GH_TOKEN" | jq -r ".assets[0].browser_download_url" | wget --output-document=$LAST_SDAPP_FILE -i -
-  curl -H "Authorization: token $GH_TOKEN" --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases" | jq -r ".[] | select(.tag_name==\"unstable\")" | jq -r ".assets[] | select(.name==\"$ARCHIVE_ZIP\")  .browser_download_url"  | wget --output-document=$ARCHIVE_ZIP -i -  &>/dev/null
-  if [ ! -f $ARCHIVE_ZIP ]; then
-    echo "Could not find a valid $ARCHIVE_ZIP from latest releases, time to tune up jq queries ?"
-    sleep 5
-    exit 1
-  fi
-  unzip -d /tmp/$ARCHIVE_ZIP $ARCHIVE_ZIP
-  cp -Ruf /tmp/$ARCHIVE_ZIP/* $M5_SD_BUILD_DIR/
-  sleep 5
-  exit 0
-fi
-
-
 my_dir="$(dirname "$0")"
 source $my_dir/functions.sh
 
