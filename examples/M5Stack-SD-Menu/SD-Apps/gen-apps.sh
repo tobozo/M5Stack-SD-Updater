f4b6b8de7753cfaacd2bf5d62cf3263d6d34e36a
tobozo <tobozo@users.noreply.github.com>
2019-05-26 18:11:50 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/gen-apps.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
   export LAST_SDAPP_FILE="SD-Apps-Folder.zip"
   #curl --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases/latest?access_token=$GH_TOKEN" | jq -r ".assets[0].browser_download_url" | wget --output-document=$LAST_SDAPP_FILE -i -
   curl -H "Authorization: token $GH_TOKEN" --retry 5 "https://api.github.com/repos/tobozo/M5Stack-SD-Updater/releases" | jq -r ".[] | select(.tag_name==\"unstable\")" | jq -r ".assets[] | select(.name==\"$ARCHIVE_ZIP\")  .browser_download_url"  | wget --output-document=$ARCHIVE_ZIP -i -
-  if [ ! -f $ARCHIVE_ZIP ]; then
+  if [ !-f $ARCHIVE_ZIP ]; then
     echo "Could not find a valid $ARCHIVE_ZIP from latest releases, time to tune up jq queries ?"
     sleep 5
     exit 1
