971f47f583bc2a0469e7b7410384b4af3269d631
tobozo <tobozo@users.noreply.github.com>
2019-05-26 18:23:10 +0200

    Preparing unstable channel

--- examples/M5Stack-SD-Menu/SD-Apps/script.sh
+++ examples/M5Stack-SD-Menu/SD-Apps/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR;
-arduino --pref "compiler.warning_level=none" --save-prefs
-arduino --pref "build.warn_data_percentage=75" --save-prefs
-arduino --pref "boardsmanager.additional.urls=https://dl.espressif.com/dl/package_esp32_index.json" --save-prefs
+arduino --pref "compiler.warning_level=none" --save-prefs   &>/dev/null
+arduino --pref "build.warn_data_percentage=75" --save-prefs   &>/dev/null
+arduino --pref "boardsmanager.additional.urls=https://dl.espressif.com/dl/package_esp32_index.json" --save-prefs   &>/dev/null
 arduino --install-boards esp32:esp32 &>/dev/null
-arduino --board $BOARD --save-prefs
+arduino --board $BOARD --save-prefs   &>/dev/null
 source $SDAPP_FOLDER/gen-menu.sh
 source $SDAPP_FOLDER/gen-apps.sh
 
