4698e773cf6d98489bf2d215bf11fc117872d643
tobozo <tobozo@users.noreply.github.com>
2025-09-09 11:08:30 +0200

    disabled chimera core

--- examples/Test/build_test/main/main.cpp
+++ examples/Test/build_test/main/main.cpp
@@ -5,9 +5,7 @@
 
 #if defined TEST_LGFX
   #include "../../../LGFX-SDLoader-Snippet/LGFX-SDLoader-Snippet.ino"
-#elif defined TEST_M5Core2
-  #include "../../../M5Core2-SDLoader-Snippet/M5Core2-SDLoader-Snippet.ino"
-#elif defined TEST_M5Stack || defined TEST_S3Box
+#elif defined TEST_M5Core2 || defined TEST_M5Stack || defined TEST_S3Box
   #include "../../../M5Stack-SDLoader-Snippet/M5Stack-SDLoader-Snippet.ino"
 #elif defined TEST_M5StickC
   #include "../../../M5StickC-SPIFFS-Loader-Snippet/M5StickC-SPIFFS-Loader-Snippet.ino"
